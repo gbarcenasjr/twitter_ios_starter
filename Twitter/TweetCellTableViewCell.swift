31f4088428092cc4941989361e84272ef7772c1c
Gerardo Barcenas Jr <gbarcenasjr@gmail.com>
2021-03-07 23:33:54 -0800

    Update 1, debugging required

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Gerardo Barcenas Jr. on 3/7/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
